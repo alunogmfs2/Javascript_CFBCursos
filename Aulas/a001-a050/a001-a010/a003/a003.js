ae81d9d9d68bd539ab0a2d7e9903ef7701680d29
alunogmfs2 <134213951+alunogmfs2@users.noreply.github.com>
2024-08-01 12:41:11 +0200

    Aula03 + 04

--- Aulas/a001-a050/a001-a010/a003/a003.js
+++ Aulas/a001-a050/a001-a010/a003/a003.js
@@ -1,1 +1,21 @@
 "use strict"
+
+/**
+    function teste() {
+    let nome = "Bruno"
+    if (true) {
+        console.log(`Dentro do if de Teste ${nome}`)
+    }
+    console.log(`Dentro de Teste ${nome}`)
+}
+
+teste()
+
+console.log(`Fora de Teste ${nome}`) //! Erro: Variavel fora de Escopo
+*/
+
+let nome = "Bruno"
+nome = "CFB Cursos"
+const curso = "Javascript"
+
+console.log(curso)
---
+++ Aulas/a001-a050/a001-a010/a004/a004.js
@@ -0,0 +1,22 @@
+"use strict"
+
+/**
+ * +
+ * -
+ * *
+ * /
+ * %
+ * ++
+ * --
+ * +=
+ * -=
+ * *=
+ * /=
+ * %=
+ */
+
+let num1 = 5, num2 = 10, res = 0
+
+res = num2 / num1
+
+console.log(`Resultado da Soma: ${res}`)
